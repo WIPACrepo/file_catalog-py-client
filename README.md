c132303b05ff5626d83ae99a9001632488d44ca3
Jan Oertlin <oe@oenet.org>
2016-10-18 13:52:21 -0500

    * Added `create_file()` method * Improved error handling. If a server side error occurs an error of the type `filecatalogpyclient.Error` (or subclass) will be raised.

--- README.md
+++ README.md
@@ -20,11 +20,20 @@ In order to get the file list, just do:
 The parameters `start`, `limit`, and `query` are also supported:
 
     c.get_file_list(query = {'filesize': {'$exists': True}})
-    c.get_file_list(query = {'filesize': {'$exists': True}}, start = 42, limit 3)
+    c.get_file_list(query = {'filesize': {'$exists': True}}, start = 42, limit = 3)
+
+### Create a New File
+To create a new file (that means a new entry for the metadata for a file) one can just use the `create_file()` method.
+
+    c.create_file({'uid': '1234', 'checksum': '3d539...f5', 'locations': ['/a/path/to/a/copy/file.dat']})
+
+The passed dict needs to fulfill the requirements of the server.
 
 ### Get File Meta Data
+The metadata for a certain file can be queried by using `get_file()`. One can either query by `uid` or `mongo_id`.
 
-    c.get_file()
+    c.get_file(uid = '1234')
+    c.get_file(mongo_id = '57fd49163a7d4957ca064089')
 
 ### Delete a File
 
@@ -37,3 +46,19 @@ The parameters `start`, `limit`, and `query` are also supported:
 ### Replace a File
 
     c.replace_file()
+
+## Errors
+There are two types of errors: client side errors and server side errors. Client side errors are instances of `filecatalogpyclient.ClientError`. Server side errors are instances of `filecatalogpyclient.Error`.
+
+### `filecatalogpyclient.Error`
+`filecatalogpyclient.Error` has the attributes `code` and `message`. `code` contains the status code with which the server responded. `message` stores the message that is returned by the server.
+
+**Note:** The server usually responds with a JSON string and the error message has the key `message`. Therefore, the class tries to extract that message and tries to store only the message in the attribute `message`.
+
+Subclasses of `filecatalogpyclient.Error`:
+* `BadRequestError`: status code 400
+* `NotFoundError`: status code 404
+* `ConflictError`: status code 409
+* `TooManyRequestsError`: status code 429
+* `UnspecificServerError`: status code 500
+* `ServiceUnavailableError`: status code 503
--- file_catalog_py_client/filecatalogpyclient.py
+++ file_catalog_py_client/filecatalogpyclient.py
@@ -3,6 +3,71 @@ import requests
 import json
 import os
 
+class ClientError(Exception):
+    """
+    Errors that occur at client side.
+    """
+    pass
+
+class Error(Exception):
+    """
+    Errors that occur at server side.
+    """
+    def __init__(self, message, code, *args):
+        self.message = message
+        self.code = code
+
+        # Try to decode message if it is a json string:
+        try:
+            self.message = json.loads(self.message)['message']
+        except:
+            # If it fails, just let the message as is
+            pass
+
+        super(Error, self).__init__(self.message, code, *args)
+
+class BadRequestError(Error):
+    def __init__(self, message, *args):
+        super(BadRequestError, self).__init__(message, 400, *args) 
+
+class TooManyRequestsError(Error):
+    def __init__(self, message, *args):
+        super(TooManyRequestsError, self).__init__(message, 429, *args) 
+
+class UnspecificServerError(Error):
+    def __init__(self, message, *args):
+        super(UnspecificServerError, self).__init__(message, 500, *args) 
+
+class ServiceUnavailableError(Error):
+    def __init__(self, message, *args):
+        super(ServiceUnavailableError, self).__init__(message, 503, *args) 
+
+class ConflictError(Error):
+    def __init__(self, message, *args):
+        super(ConflictError, self).__init__(message, 409, *args) 
+
+class NotFoundError(Error):
+    def __init__(self, message, *args):
+        super(NotFoundError, self).__init__(message, 404, *args) 
+
+def error_factory(code, message):
+    """
+    Tries to find the correct `Error` class. If no class is found that corresponds to the `code`,
+    it will utilize the `Error` class it self.
+    """
+    if 'cls' not in error_factory.__dict__ or 'codes' not in error_factory.__dict__:
+        error_factory.__dict__['cls'] = Error.__subclasses__()
+        error_factory.__dict__['codes'] = [c('').code for c in error_factory.__dict__['cls']]
+
+        print "Init error_factory executed"
+
+    try:
+        # `index()` throws an `ValueError` if the value isn't found
+        i = error_factory.__dict__['codes'].index(code)
+        return error_factory.__dict__['cls'][i](message)
+    except ValueError as e:
+        return Error(message, code)
+
 class FileCatalogPyClient:
     def __init__(self, url, port = None):
         """
@@ -35,7 +100,7 @@ class FileCatalogPyClient:
             payload['limit'] = int(limit)
 
         if not isinstance(query, dict):
-            raise Exception('Argument `query` must be a dict.')
+            raise ClientError('Argument `query` must be a dict.')
 
         if query:
             payload['query'] = json.dumps(query)
@@ -51,7 +116,7 @@ class FileCatalogPyClient:
 
             return rdict
         else:
-            raise Exception(r.text)
+            raise error_factory(r.status_code, r.text)
 
     def get_file(self, mongo_id = None, uid = None):
         """
@@ -61,10 +126,10 @@ class FileCatalogPyClient:
         However, get_file_list() caches automatically all mappings that it received.
         """
         if mongo_id is None and uid is None:
-            raise Exception('You need to specify either `mongo_id` or `uid`')
+            raise ClientError('You need to specify either `mongo_id` or `uid`')
 
         if mongo_id is not None and uid is not None:
-            raise Exception('The query is ambiguous. Do not specify `uid` and `mongo_id` at the same time.')
+            raise ClientError('The query is ambiguous. Do not specify `uid` and `mongo_id` at the same time.')
 
         # If mongo_id is given, we have anything we need to do the query
 
@@ -84,18 +149,39 @@ class FileCatalogPyClient:
                     mongo_id = self._cache[uid]
                 else:
                     # :'( The uid has not been found in the file catalog
-                    raise Exception("The uid `%s` is not present in the file catalog" % uid)
+                    raise ClientError("The uid `%s` is not present in the file catalog" % uid)
         
         # Query the data 
         r = requests.get(os.path.join(self._url, 'files', mongo_id))
-        return json.loads(r.text)
 
-    def update_file(self):
+        if r.status_code == requests.codes.OK:
+            return json.loads(r.text)
+        else:
+            raise error_factory(r.status_code, r.text)
+
+    def create_file(self, metadata):
+        """
+        Tries to create a file in the file catalog.
+
+        `metadata` must be a dictionary and needs to contain at least all mandatory fields.
+
+        *Note*: The client does not check the metadata. Checks are entirely done by the server.
+        """
+        r = requests.post(os.path.join(self._url, 'files'), json.dumps(metadata))
+
+        if r.status_code == requests.codes.CREATED:
+            # Add uid/mongo_id to cache
+            rdict = json.loads(r.text)
+            self._cache[metadata['uid']] = os.path.basename(rdict['file'])
+        else:
+            raise error_factory(r.status_code, r.text)
+
+    def update_file(self, mongo_id = None, uid = None):
         pass
 
-    def replace_file(self):
+    def replace_file(self, mongo_id = None, uid = None):
         pass
 
-    def delete_file(self):
+    def delete_file(self, mongo_id = None, uid = None):
         pass
 
